897d45c4293bb2f7be038778b44ac28b6d2b2a88
Demid <114720526+iceberkut@users.noreply.github.com>
2023-09-14 19:21:13 +0500

    Delete 00-07.py

--- 00-07.py
+++
@@ -1,4 +0,0 @@
-film = input("Enter your Film: ")
-cinema = input("Enter your Cinema: ")
-time = input("Enter your Time: ")
-print("Билет на '" + film + "' в кинотеатре '" + cinema + "' на " + time + " забронирован")
